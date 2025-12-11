11bee3732eabdaf4bf6ff2371766350fb656baee
saverio mandarano <saverio.mandarano1@gmail.com>
2025-12-11 17:29:13 +0100

    calcolo del prezzo, con eventuali sconti applicati...

--- js/script.js
+++ js/script.js
@@ -1,17 +1,3 @@
-// const prezzoIniziale= km * 0.21;
-// let sconto= 0;
-
-// if (età < 18){
-//     sconto = 0.20;
-// } 
-// else if (età > 65) {
-//     sconto = 0.40;
-// } 
-// const prezzoFinale = (prezzoIniziale * (1 - sconto)).toFixed(2);
-// console.log(`Il prezzo del biglietto è di: € ${prezzoFinale}` );
-
-
-
 // MILESTONE 1:
 // Iniziamo implementando il programma senza alcuna estetica: 
 // usando esclusivamente due input e un bottone (non stilizzati),
@@ -21,11 +7,30 @@
 const form = document.querySelector(`form`);
 
 form.addEventListener(`submit`, function(e) {
+    // prevengo comportamento di deafult del submit...
     e.preventDefault();
 
+    // estraggo dati di input dell'utente...
     const km = Number(Number(document.getElementById(`userKm`).value).toFixed(2));
     const age = Number(Number(document.getElementById(`userAge`).value).toFixed(2));
     console.log(`km: ${km}`);
     console.log(`age: ${age}`);
-    console.log(typeof km);
+    console.log(`typeof km/age`, typeof km, typeof age);
+
+    //calcolo del prezzo, con eventuali sconti applicati...
+    let price = km * 0.21;
+    let discount = 0;
+
+    if (age < 18){
+        discount = 0.20;
+    } 
+    else if (age > 65) {
+        discount = 0.40;
+    } 
+
+    price = Number((price * (1 - discount)).toFixed(2));
+    console.log(`typeof price`, typeof price);
+    console.log(`Il prezzo del biglietto è di: € ${price}` );
+
+
 });
