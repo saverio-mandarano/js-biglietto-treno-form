779c8420c464da6884752c4090a59b94607311cc
saverio mandarano <saverio.mandarano1@gmail.com>
2025-12-11 17:02:10 +0100

    prendo i valori di input dopo aver premuto submit

--- js/script.js
+++ js/script.js
@@ -1,24 +1,14 @@
-// Il programma dovrà chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero.
-const km= parseFloat(prompt("Inserisci il numero di chilometri che vuoi percorrere:"))
-const età= parseInt(prompt("Adesso inserisci l'età del passeggero:"))
+// const prezzoIniziale= km * 0.21;
+// let sconto= 0;
 
-// Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
-// Il prezzo del biglietto è definito in base ai km (0.21 € al km)
-const prezzoIniziale= km * 0.21;
-let sconto= 0;
-
-// va applicato uno sconto del 20% per i minorenni
-if (età < 18){
-    sconto = 0.20;
-} 
-// va applicato uno sconto del 40% per gli over 65.
-else if (età > 65) {
-    sconto = 0.40;
-} 
-// L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo).
-const prezzoFinale = (prezzoIniziale * (1 - sconto)).toFixed(2);
-// Output partite da semplice log, e poi se volete stampate in pagina come sapete fare.
-console.log(`Il prezzo del biglietto è di: € ${prezzoFinale}` );
+// if (età < 18){
+//     sconto = 0.20;
+// } 
+// else if (età > 65) {
+//     sconto = 0.40;
+// } 
+// const prezzoFinale = (prezzoIniziale * (1 - sconto)).toFixed(2);
+// console.log(`Il prezzo del biglietto è di: € ${prezzoFinale}` );
 
 
 
@@ -32,4 +22,9 @@ const form = document.querySelector(`form`);
 
 form.addEventListener(`submit`, function(e) {
     e.preventDefault();
+
+    const km = document.getElementById(`userKm`).value;
+    const age = document.getElementById(`userAge`).value;
+    console.log(`km: ${km}`);
+    console.log(`age: ${age}`);
 });
