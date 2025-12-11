0d4326aa737c37296c812766e93ad48e0a9b22e3
saverio mandarano <saverio.mandarano1@gmail.com>
2025-12-11 18:12:02 +0100

    // check di controllo input

--- js/script.js
+++ js/script.js
@@ -10,15 +10,25 @@ form.addEventListener(`submit`, function(e) {
     // prevengo comportamento di deafult del submit...
     e.preventDefault();
 
+
     // estraggo dati di input dell'utente...
     const inputKm = Number(document.getElementById(`userKm`).value.trim().replace(`,`, `.`)); //km in input, massimo due decimali e cambio virgole in punti sennò non funziona
     const km = Number(inputKm.toFixed(2)); //arrotondo a centesimi
    
     const age = parseInt(document.getElementById(`userAge`).value.trim().replace(`,`, `.`)); //age in input, numero intero
 
+
+    // check di controllo input
+    if (isNaN(km) || isNaN(age) || km <= 0 || age <= 0) {
+        console.log(`Input not valid. Please try again...`);
+        return;
+    }
+
+
     console.log(`km: ${km}`);
     console.log(`age: ${age}`);
     console.log(`typeof km/age`, typeof km, typeof age);
+
 
     //calcolo del prezzo, con eventuali sconti applicati...
     let price = km * 0.21;
@@ -34,6 +44,5 @@ form.addEventListener(`submit`, function(e) {
     price = Number((price * (1 - discount)).toFixed(2));
     console.log(`typeof price`, typeof price);
     console.log(`Il prezzo del biglietto è di: € ${price}` );
-
 
 });
